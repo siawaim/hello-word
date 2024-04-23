4e9cbc4291b28b5f4f8704e7dd60da2dde1ab7c0
Cliver Omar Flores Pacheco <cliveromar1234@gmail.com>
2024-04-23 11:05:35 -0500

    Colab +++

--- ParallelMangaTranslator.py
+++ ParallelMangaTranslator.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     modelo_inpaint = MODELOS_INPAINT[1]
     print(f"Modelo inpaint seleccionado: {modelo_inpaint}")
-    batch_size = 1 # Tamaño de lote -> Si hay 24 imágenes entonces se crearán 3 procesos de 8 imágenes cada uno
+    batch_size = 8 # Tamaño de lote -> Si hay 24 imágenes entonces se crearán 3 procesos de 8 imágenes cada uno
     idioma_entrada = "Japonés"
     idioma_salida = "Español"
     ruta_carpeta_entrada = "Dataset"
